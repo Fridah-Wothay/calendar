a92301de4b93a8c8b01d4ad02177c45a1500ff28
Fridah-Wothay <wothayafridahkamau@gmail.com>
2024-04-06 21:42:32 +0300

    add changes

--- script.js
+++ script.js
@@ -28,3 +28,32 @@ const months = [
 ];
 
 
+
+
+
+    // Function to generate the calendar
+const manipulate = () => {
+ 
+    // Get the first day of the month
+    let dayone = new Date(year, month, 1).getDay();
+ 
+    // Get the last date of the month
+    let lastdate = new Date(year, month + 1, 0).getDate();
+ 
+    // Get the day of the last date of the month
+    let dayend = new Date(year, month, lastdate).getDay();
+ 
+    // Get the last date of the previous month
+    let monthlastdate = new Date(year, month, 0).getDate();
+ 
+    // Variable to store the generated calendar HTML
+    let lit = "";
+ 
+    // Loop to add the last dates of the previous month
+    for (let i = dayone; i > 0; i--) {
+        lit +=
+            `<li class="inactive">${monthlastdate - i + 1}</li>`;
+    }
+}
+
+
